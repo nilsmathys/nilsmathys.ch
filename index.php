9ac03929aeaf199dc9b93488056404203837d2a4
mathynil <mathynil@students.zhaw.ch>
2020-02-16 14:45:25 +0100

    darkmode verbesser: graues Profilbild Formular verbessert: Fehlermeldung anzeigen

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
+$mode = isset($_COOKIE['mode']) ? $_COOKIE['mode'] : 'light';
+
+if (isset($_GET["toggle"])) {
+    if ($mode == "light") {
+        setcookie('mode', 'dark', strtotime('+1 year'));
+    } else {
+        setcookie('mode', 'light', strtotime('+1 year'));
+    }
+    header('Location: https://nilsmathys.ch');
+}
+
 $cfg = [
     'recaptcha_site' => '',
     'recaptcha_secret' => ''
@@ -6,27 +17,50 @@ $cfg = [
 if (file_exists('local.php')) {
     $cfg = include 'local.php';
 }
+$error = [];
+$name = $_POST["name"] ?: '';
+$email = $_POST["email"] ?: '';
+$message = $_POST["message"] ?: '';
+$sent = false;
 
-if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"]) && !empty($_POST["g-recaptcha-response"])) {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-    $recaptcha = $_POST["g-recaptcha-response"];
-    $url = 'https://www.google.com/recaptcha/api/siteverify';
-    $options = [
-        'http' => [
-            'method' => 'POST',
-            'content' => http_build_query([
-                'secret' => $cfg['recaptcha_secret'],
-                'response' => $recaptcha
-            ])
-        ]
-    ];
-    $context = stream_context_create($options);
-    $verify = file_get_contents($url, false, $context);
-    $checkCaptcha = json_decode($verify);
-    if ($checkCaptcha->success) {
-        mail("info@nilsmathys.ch", "Kontaktformular", "name: " . $name . "\nemail: " . $email . "\nmessage: " . $message);
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (empty($name)) {
+        $error[] = "name";
+    }
+    if (empty($email)) {
+        $error[] = "email";
+    }
+    if (empty($message)) {
+        $error[] = "message";
+    }
+
+    if (sizeof($error) == 0) {
+        $recaptcha = $_POST["g-recaptcha-response"];
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $options = [
+            'http' => [
+                'method' => 'POST',
+                'content' => http_build_query([
+                    'secret' => $cfg['recaptcha_secret'],
+                    'response' => $recaptcha
+                ])
+            ]
+        ];
+        $context = stream_context_create($options);
+        $verify = file_get_contents($url, false, $context);
+        $checkCaptcha = json_decode($verify);
+        if ($checkCaptcha->success) {
+            if (mail("info@nilsmathys.ch", "Kontaktformular", "name: " . $name . "\nemail: " . $email . "\nmessage: " . $message)) {
+                $name = '';
+                $email = '';
+                $message = '';
+                $sent = true;
+            } else {
+                $error[] = "mail";
+            }
+        } else {
+            $error[] = "recaptcha";
+        }
     }
 }
 ?>
@@ -53,7 +87,8 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
 
     <meta name="author" content="Nils Mathys">
     <meta name="publisher" content="">
-    <meta name="keywords" content="Nils, Mathys, Software Engineer, Engineering, Software-Entwicklung, Informatikstudent">
+    <meta name="keywords"
+          content="Nils, Mathys, Software Engineer, Engineering, Software-Entwicklung, Informatikstudent">
     <meta name="description" content="Nils Mathys, Software Engineer in study - Kontaktiere mich hier">
     <meta name="robots" content="index,follow">
     <meta name="copyright" content="Nils Mathys, nilsmathys.ch">
@@ -64,14 +99,14 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/all.min.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-    <link href="" rel="stylesheet" id="darkmode">
+    <?php if($mode == "dark") { ?>
+    <link href="css/darkmode.css" rel="stylesheet" id="darkmode">
+    <?php } ?>
 
     <script src="js/jquery-3.4.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/index.js"></script>
 </head>
 <body>
-<div id="darkmode-toggle">🌓</div>
 <div class="container my-4">
     <div class="row">
         <div class="col-md-6 m-auto">
@@ -80,7 +115,10 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
                     <div class="row">
                         <div class="m-auto col-6 col-md-5 col-lg-4 col-xl-3">
                             <a href="https://nilsmathys.ch">
-                                <img src="img/profilephoto.jpg" class="w-100 my-4 rounded-circle" id="profilephoto">
+                                <img src="img/profilephoto.jpg" class="w-100 my-4 rounded-circle darkmode-none"
+                                     id="profilephoto">
+                                <img src="img/profilephoto-darkmode.jpg"
+                                     class="w-100 my-4 rounded-circle d-none darkmode-show" id="profilephoto">
                             </a>
                         </div>
                     </div>
@@ -91,23 +129,33 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
                     <div class="mt-4 text-left" id="content">
                         <p style="text-align: center;"><span style="font-size: 14pt;"><strong>Kontaktiere mich</strong></span>
                         </p>
+                        <?php if ($sent) { ?>
+                            <div class="alert alert-success" role="alert">
+                                Nachricht wurde versendet!
+                            </div>
+                        <?php } elseif (sizeof($error) > 0) { ?>
+                            <div class="alert alert-danger" role="alert">
+                                Bitte prüfe deine Eingaben!
+                            </div>
+                        <?php } ?>
                         <form method="post">
                             <div class="form-group">
-                                <input type="text" name="name" class="form-control " id="input_name" value=""
+                                <input type="text" name="name" class="form-control <?php if (in_array('name', $error)) { echo "is-invalid"; } ?>" id="input_name" value="<?= $name; ?>"
                                        placeholder="Name">
                             </div>
                             <div class="form-group">
-                                <input type="email" name="email" class="form-control " id="input_email" value=""
+                                <input type="email" name="email" class="form-control <?php if (in_array('email', $error)) { echo "is-invalid"; } ?>" id="input_email" value="<?= $email; ?>"
                                        placeholder="E-Mail">
                             </div>
                             <div class="form-group">
-                                <textarea class="form-control " name="message" id="input_message"
-                                          placeholder="Nachricht"></textarea>
+                                <textarea class="form-control <?php if (in_array('message', $error)) { echo "is-invalid"; } ?>" name="message" id="input_message"
+                                          placeholder="Nachricht"><?= $message; ?></textarea>
                             </div>
                             <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-                            <div class="g-recaptcha mb-2 "
+                            <div class="g-recaptcha mb-2 " <?php if ($mode == "dark") { echo 'data-theme="dark"'; } ?>
                                  data-sitekey="<?php echo $cfg['recaptcha_site']; ?>"></div>
-                            <button type="submit" name="send" class="btn btn-primary btn-lg btn-block" value="">Senden</button>
+                            <button type="submit" name="send" class="btn btn-primary btn-lg btn-block" value="">Senden
+                            </button>
                         </form>
                     </div>
                     <div class="border-top mt-4 py-4">
@@ -116,21 +164,27 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
                             <i class="fab fa-linkedin-in"></i>
                         </a>
                         <a href="https://github.com/nilsmathys" target="_blank"
-                           class="btn btn-light fa-2x rounded-circle px-3 ml-2">
+                           class="btn btn-light fa-2x rounded-circle px-3 mx-2">
                             <i class="fab fa-github"></i>
+                        </a>
+                        <a href="?toggle"
+                           class="btn btn-light fa-2x rounded-circle px-3 ml-2">
+                            <i class="fa fa-adjust"></i>
                         </a>
                     </div>
                     <div class="border-top pt-4 footer">
                         <p>Copyright &copy; 2020 by Nils Mathys</p>
                         <div class="border-top mt-2 py-2">
                             <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-info btn-sm btn-block" data-toggle="modal" data-target="#privacy-policy">
+                            <button type="button" class="btn btn-info btn-sm btn-block" data-toggle="modal"
+                                    data-target="#privacy-policy">
                                 Datenschutzerklärung
                             </button>
 
                             <!-- Modal -->
-                            <div class="modal fade" id="privacy-policy" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
+                            <div class="modal fade" id="privacy-policy" tabindex="-1" role="dialog"
+                                 aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="exampleModalLongTitle">Datenschutzerklärung</h5>
@@ -139,10 +193,12 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
                                             </button>
                                         </div>
                                         <div class="modal-body">
-                                            <?php echo file_get_contents('text/datenschutz.html');?>
+                                            <?php echo file_get_contents('text/datenschutz.html'); ?>
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Schliessen</button>
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                                                Schliessen
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
@@ -150,13 +206,15 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
                         </div>
                         <div>
                             <!-- Button trigger modal -->
-                            <button type="button" class="btn btn-info btn-sm btn-block" data-toggle="modal" data-target="#impressum">
+                            <button type="button" class="btn btn-info btn-sm btn-block" data-toggle="modal"
+                                    data-target="#impressum">
                                 Impressum
                             </button>
 
                             <!-- Modal -->
-                            <div class="modal fade" id="impressum" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-                                <div class="modal-dialog" role="document">
+                            <div class="modal fade" id="impressum" tabindex="-1" role="dialog"
+                                 aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-scrollable" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
                                             <h5 class="modal-title" id="exampleModalLongTitle">Impressum</h5>
@@ -165,10 +223,12 @@ if (!empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"
                                             </button>
                                         </div>
                                         <div class="modal-body">
-                                            <?php echo file_get_contents('text/impressum.html');?>
+                                            <?php echo file_get_contents('text/impressum.html'); ?>
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Schliessen</button>
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">
+                                                Schliessen
+                                            </button>
                                         </div>
                                     </div>
                                 </div>
--- js/index.js
+++ js/index.js
@@ -3,15 +3,22 @@ $(function() {
 
     $('#darkmode-toggle').click(function() {
         toggleDarkmode(($('#darkmode').attr('href') !== 'css/darkmode.css'));
+        return false;
     });
 
     function toggleDarkmode(setDark) {
         if (setDark) {
             localStorage.setItem('darkmode', '1');
             $('#darkmode').attr('href', 'css/darkmode.css');
+            grecaptcha.render("recaptcha", {
+                "theme": "dark"
+            })
         } else {
             localStorage.setItem('darkmode', '0');
             $('#darkmode').attr('href', '');
+            grecaptcha.render("recaptcha", {
+                "theme": "light"
+            })
         }
     }
 });
